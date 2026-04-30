ea52e53ae627efa5651a4c3abd6418237caf6739
Albertcs7 <albertacs2004@gmail.com>
2026-04-30 12:11:13 +0530

    S3earch bar implemented

--- apps/frontend/src/components/searchBar/SearchBar.vue
+++ apps/frontend/src/components/searchBar/SearchBar.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { computed, onBeforeUnmount, onMounted, ref } from "vue";
-import { watch } from "vue";
+import { computed, onBeforeUnmount, onMounted, ref, watch } from "vue";
 
 const query = ref("");
 
